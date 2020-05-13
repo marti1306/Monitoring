162934ed0eedc8892a83757ceab99d78f0cb81f5
marti1306 <62665243+marti1306@users.noreply.github.com>
2020-05-13 17:56:05 +0200

    Update R_code_faPAR.r

--- R_code_faPAR.r
+++ R_code_faPAR.r
@@ -16,3 +16,25 @@ pdf("faPAR.pdf")
 levelplot(faPAR10) #in north emisphere the fapar is not that high as NDVI --> we're considering with this indicator not only biomass but also carbon intake
 
 dev.off()
+
+###2nd day
+
+setwd("C:/lab/")
+load("faPAR.RData")
+
+library(raster)
+library(rasterdiv)
+
+# the original faPAR from Copernicus is 2GB
+
+#let's see how much space is needed for an 8bit set
+#let's write a raster outside R
+writeRaster(copNDVI, "copNDVI.tif")
+#5.3 MB
+
+library(rasterVis)
+
+#exercise: make the levelplot of the faPAR
+faPAR10 <- raster("faPAR10.tif")
+levelplot(faPAR10)
+
